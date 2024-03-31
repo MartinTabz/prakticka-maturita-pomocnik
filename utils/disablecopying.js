03aaf5732c0a2e6b4bd10eff765552da2a6cccb3
Martin Šíl <mail@martinsil.cz>
2024-03-31 12:33:06 +0200

    Responsive styles for chapter content

--- utils/disablecopying.js
+++ utils/disablecopying.js
@@ -4,28 +4,29 @@ import { useEffect } from "react";
 
 export default function DisableCopying({ children }) {
 	useEffect(() => {
-		const ctrlShiftKey = (e, keyCode) => {
-			return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-		};
+		if (process.env.NEXT_PUBLIC_ENV == "production") {
+			const ctrlShiftKey = (e, keyCode) => {
+				return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+			};
 
-		const handleKeyDown = (e) => {
-			// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-			if (
-				e.keyCode === 123 ||
-				ctrlShiftKey(e, "I") ||
-				ctrlShiftKey(e, "J") ||
-				ctrlShiftKey(e, "C") ||
-				(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-			) {
-				e.preventDefault();
-			}
-		};
+			const handleKeyDown = (e) => {
+				if (
+					e.keyCode === 123 ||
+					ctrlShiftKey(e, "I") ||
+					ctrlShiftKey(e, "J") ||
+					ctrlShiftKey(e, "C") ||
+					(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+				) {
+					e.preventDefault();
+				}
+			};
 
-		document.addEventListener("keydown", handleKeyDown);
+			document.addEventListener("keydown", handleKeyDown);
 
-		return () => {
-			document.removeEventListener("keydown", handleKeyDown);
-		};
+			return () => {
+				document.removeEventListener("keydown", handleKeyDown);
+			};
+		}
 	}, []);
 
 	return <div onContextMenu={(e) => e.preventDefault()}>{children}</div>;
