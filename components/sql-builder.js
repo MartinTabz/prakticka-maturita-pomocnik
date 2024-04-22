672a49e17dbd2d9c9f43f1b8b566bfb11c2a7c15
Martin Šíl <mail@martinsil.cz>
2024-04-22 02:28:14 +0200

    SQL builder actions

--- components/sql-builder.js
+++ components/sql-builder.js
@@ -6,10 +6,16 @@ import { useNotifications } from "@utils/notificationcontext";
 
 export default function SqlBuilder() {
 	const { newError } = useNotifications();
-
-	const [tables, setTables] = useState([
-		{ name: "Tabulka1", attributes: [{ name: "id", type: "int" }] },
+	const [actions, setActions] = useState([
+		{ name: "SELECT", desc: "Vypsat" },
+		{ name: "INSERT", desc: "Vložit" },
+		{ name: "DELETE", desc: "Smazat" },
+		{ name: "UPDATE", desc: "Upravit" },
 	]);
+
+	const [tables, setTables] = useState([]);
+	const [activeAction, setActiveAction] = useState("");
+	const [definition, setDefinition] = useState("");
 
 	const deleteAttribute = (tableIndex, attributeIndex) => {
 		setTables((prevTables) => {
@@ -58,7 +64,7 @@ export default function SqlBuilder() {
 		} else {
 			const newTable = {
 				name: tableName,
-				attributes: [{ name: "id", type: "int" }],
+				attributes: [{ name: "id", type: "pk" }],
 			};
 			setTables((prevTables) => [...prevTables, newTable]);
 		}
@@ -66,9 +72,15 @@ export default function SqlBuilder() {
 		e.target[0].value = "";
 	};
 
-	const handleNewAttribute = (e, tableIndex, dataType) => {
+	const handleNewAttribute = (e, tableIndex) => {
 		e.preventDefault();
 		const attributeName = e.target[0].value;
+		const typeName = e.target[1].value;
+
+		if (!attributeName || !typeName) {
+			newError("Jméno a typ je povinný");
+			return;
+		}
 
 		setTables((prevTables) => {
 			const updatedTables = [...prevTables];
@@ -81,7 +93,7 @@ export default function SqlBuilder() {
 				} else {
 					const updatedAttributes = [
 						...tableToUpdate.attributes,
-						{ name: attributeName, dataType: "" },
+						{ name: attributeName, type: typeName },
 					];
 					updatedTables[tableIndex] = {
 						...tableToUpdate,
@@ -95,6 +107,10 @@ export default function SqlBuilder() {
 		e.target[0].value = "";
 	};
 
+	const handleSend = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<section>
 			<div>
@@ -104,34 +120,82 @@ export default function SqlBuilder() {
 				</div>
 				<section>
 					<h2>Tabulky</h2>
-					{tables.map((t, index) => (
-						<div key={index}>
-							<div>
-								<h3>{t.name}</h3>
-								<button onClick={() => handleDeleteTable(index)}>X</button>
-							</div>
+					<div>
+						{tables.map((t, index) => (
+							<div key={index}>
+								<div>
+									<h3>{t.name}</h3>
+									<button onClick={() => handleDeleteTable(index)}>X</button>
+								</div>
 
-							<div>
-								{t.attributes.map((a, i) => (
-									<div index={i}>
-										<span>{a.name}</span>
-										<i>({a.type})</i>
-										<button onClick={() => deleteAttribute(index, i)}>X</button>
-									</div>
-								))}
-								<form onSubmit={(e) => handleNewAttribute(e, index)}>
-									<input placeholder="Nový atribut" type="text" />
-									// Dropdown
-									<button type="submit">+</button>
-								</form>
+								<div>
+									{t.attributes.map((a, i) => (
+										<div index={i}>
+											<span>{a.name}</span>
+											<i>({a.type})</i>
+											<button onClick={() => deleteAttribute(index, i)}>
+												X
+											</button>
+										</div>
+									))}
+									<form onSubmit={(e) => handleNewAttribute(e, index)}>
+										<input placeholder="Nový atribut" type="text" />
+										<select>
+											<option value="int">Číslo</option>
+											<option value="string">Text</option>
+											<option value="bool">Ano/Ne</option>
+											<option value="pk">Primární klíč</option>
+											<option value="fk">Cizí klíč</option>
+										</select>
+										<button type="submit">+</button>
+									</form>
+								</div>
 							</div>
-						</div>
-					))}
-					<form onSubmit={handleNewTable}>
-						<input placeholder="Název nové tabulky" type="text" />
-						<button type="submit">+</button>
-					</form>
+						))}
+						<form onSubmit={handleNewTable}>
+							<input placeholder="Název nové tabulky" type="text" />
+							<button type="submit">+</button>
+						</form>
+					</div>
 				</section>
+				<section>
+					<h2>Akce</h2>
+					<div>
+						{actions.map((action, index) => (
+							<div
+								key={index}
+								onClick={() => setActiveAction(action.name)}
+								style={{
+									border: activeAction == action.name && "2px solid grey",
+								}}
+							>
+								<h3>{action.name}</h3>
+								<p>{action.desc}</p>
+							</div>
+						))}
+					</div>
+				</section>
+				<section>
+					<h2>Definice</h2>
+					<textarea
+						value={definition}
+						onChange={(e) => {
+							setDefinition(e.target.value);
+						}}
+						placeholder={
+							activeAction == "SELECT"
+								? "Vypiš z tabulky X počet Y, které jsou starší než Z"
+								: activeAction == "INSERT"
+								? "Vlož do tabulky X nový záznam, který bude mít hodnoty: Y = y, Z = z"
+								: activeAction == "DELETE"
+								? "Smaž záznam, kde id je 3"
+								: activeAction == "UPDATE"
+								? "U tabulky X uprav atribut Y na hodnotu Z"
+								: "Napiš co chceš aby příkaz dělal"
+						}
+					/>
+				</section>
+				<button onClick={handleSend}>Vytvořit</button>
 			</div>
 		</section>
 	);
